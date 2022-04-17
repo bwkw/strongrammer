93797df37af4d656a469e3530a6a8ed3f0585f7a
S.Naito <na.shota.1139@gmail.com>
2022-04-17 16:32:42 +0900

    feat: topページの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HtmlIndex from 'components/html/index'
 import HtmlShow from 'components/html/show'
+import Top from 'components/top'
 
 const App: React.VFC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Top />}></Route>
         <Route path="/html" element={<HtmlIndex />}></Route>
         <Route path="/html/:category" element={<HtmlShow />}></Route>
       </Routes>
--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,23 +1,21 @@
-import { VFC } from 'react'
+import {VFC} from 'react'
 
 import Grid from '@mui/material/Grid'
 
-import BackButton from 'components/elements/Button/back'
 import LinkButton from 'components/elements/Button/link'
 
-const Index: VFC = () => {
+const Top: VFC = () => {
   return (
     <>
-      <BackButton url={'/'} />
       <Grid item container xs={12}>
         <Grid item xs={1} />
         <Grid item xs={6}>
-          <h1>HTML</h1>
-          <LinkButton url="/html/tag" letter="タグ" />
+          <h1>Strongrammer</h1>
+          <LinkButton url="/html" letter="HTML" />
         </Grid>
       </Grid>
     </>
   )
 }
 
-export default Index
+export default Top
