d5d3d55199160587fc30fd353d5e3615b18bf663
S.Naito <na.shota.1139@gmail.com>
2022-04-23 09:06:44 +0900

    feat: QUizLogsReducer用Typeの定義

---
+++ src/type/quizLogsReducerType.tsx
@@ -0,0 +1,22 @@
+// quizLogsReducerに関するtypeを定義
+import { ADD_QUIZ_LOG } from 'store/actions/quizLogsReducer'
+
+export type ColumnsType = {
+  id: string
+  label: string
+  minWidth: number
+}
+
+export type StateType = {
+  category: string
+  correctWrongJudgement: '○' | '×' | ''
+  dateTime: string
+}
+
+export type StatesType = {
+  states: StateType[]
+}
+
+export type ActionType = StateType & {
+  type: typeof ADD_QUIZ_LOG
+}
