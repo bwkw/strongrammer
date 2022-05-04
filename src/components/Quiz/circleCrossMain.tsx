eb28675b5197fd00d6fb1c68aa693195276151f0
S.Naito <na.shota.1139@gmail.com>
2022-05-04 22:47:04 +0900

    feat: ○×クイズのformatの作成

--- src/components/Quiz/circleCrossMain.tsx
+++ src/components/Quiz/circleCrossMain.tsx
@@ -25,10 +25,10 @@ const CircleCrossQuizMain: VFC = () => {
 
               <div className="flex flex-col items-center">
                 <div className="flex mt-4 space-x-12">
-                  <button className="inline-flex items-center py-2 px-8 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                  <button className="inline-flex items-center py-2 px-12 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
                     ○
                   </button>
-                  <button className="inline-flex items-center py-2 px-8 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                  <button className="inline-flex items-center py-2 px-12 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
                     ×
                   </button>
                 </div>
