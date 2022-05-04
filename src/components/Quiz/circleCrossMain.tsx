bc1857ce9a18b107088f23a0f281374b62ee11fa
S.Naito <na.shota.1139@gmail.com>
2022-05-04 22:46:37 +0900

    feat: ○×クイズのformatの作成

--- src/components/Quiz/circleCrossMain.tsx
+++ src/components/Quiz/circleCrossMain.tsx
@@ -23,9 +23,16 @@ const CircleCrossQuizMain: VFC = () => {
                 so far, in reverse chronological order.
               </p>
 
-              <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-              </button>
+              <div className="flex flex-col items-center">
+                <div className="flex mt-4 space-x-12">
+                  <button className="inline-flex items-center py-2 px-8 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                    ○
+                  </button>
+                  <button className="inline-flex items-center py-2 px-8 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                    ×
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         )}
