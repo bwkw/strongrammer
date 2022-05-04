4e4a7af2b47b1d3927e11b48f149a0f2127a05f3
S.Naito <na.shota.1139@gmail.com>
2022-05-04 22:35:15 +0900

    feat: ○×クイズのformatの作成

--- src/components/Quiz/circleCrossMain.tsx
+++ src/components/Quiz/circleCrossMain.tsx
@@ -14,9 +14,18 @@ const CircleCrossQuizMain: VFC = () => {
         )}
         {quizStartFlag && (
           <div className="relative col-start-3 col-span-8">
-            <div className="absolute inset-x-0 top-0 h-16 bg-gray-600">Q1.</div>
-            <div className="absolute inset-x-0 top-16 h-64 bg-gray-200">
-              テスト
+            <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Q1.
+              </h5>
+              <p className="mb-7 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+
+              <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+              </button>
             </div>
           </div>
         )}
