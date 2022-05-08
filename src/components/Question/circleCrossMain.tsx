727eb9cdba6fb71855415314e36152f1ed23d563
S.Naito <na.shota.1139@gmail.com>
2022-05-08 10:01:08 +0900

    refactor: quizコンポーネントの分割

--- src/components/Question/circleCrossMain.tsx
+++ src/components/Question/circleCrossMain.tsx
@@ -1,15 +1,13 @@
 import { VFC } from 'react'
 
-type CircleCrossQuestionMainProps = {}
-
-const CircleCrossQuestionMain: VFC<CircleCrossQuestionMainProps> = () => {
+const CircleCrossQuestionMain: VFC = () => {
   const quizList = [
     'marginを上30px, 下20px, 左15px, 右15px作る指定方法は、\n「margin: 30px 15px 20px」である。',
   ]
 
   return (
     <>
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="mb-2 text-left text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Q1.
       </h5>
       <div className="mb-7 font-normal text-gray-700 text-center dark:text-gray-400 whitespace-pre-line">
--- src/components/Quiz/circleCrossMain.tsx
+++ src/components/Quiz/circleCrossMain.tsx
@@ -18,8 +18,8 @@ const CircleCrossQuizMain: VFC = () => {
           <div className="relative col-start-3 col-span-8">
             <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex flex-col items-center">
+                <CircleCrossQuestionMain />
                 <div className="flex mt-4 space-x-12">
-                  {CircleCrossQuestionMain}
                   <button className="inline-flex items-center py-2 px-12 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
                     ○
                   </button>
