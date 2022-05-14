afdabb0dfa1068ef5cb293c6f1ac1d6f0e27b9e7
S.Naito <na.shota.1139@gmail.com>
2022-05-14 15:16:20 +0900

    feat: CSS Margin Quiz作成

--- src/components/Quiz/CSS/margin.tsx
+++ src/components/Quiz/CSS/margin.tsx
@@ -6,10 +6,11 @@ import QuizCommonPart from 'components/Quiz/commonPart'
 const Margin: VFC = () => {
   const quizList = [
     'marginとは要素の内側の余白を指定するものである。',
-    '値が二つ指定された場合、一つ目のmarginは左右、二つ目のmarginは上下に適用される。',
+    'marginとpaddingはどちらを用いても同じ余白を作る',
+    'marginの値が2つ指定された場合、1つ目のmarginは左右、1つ目のmarginは上下に適用される。',
     'marginを上30px, 下20px, 左15px, 右15px作る指定方法は、\n「margin: 30px 15px 20px」である。',
   ]
-  const answerList = ['×', '×', '○']
+  const answerList = ['×', '×', '×', '○']
   return (
     <>
       <QuizCommonPart language={'CSS'} title={'Margin'} />
